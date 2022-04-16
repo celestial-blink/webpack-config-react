3509e17df550540bda1151bf3a56879b308f9a79
takeshi <kkrakenbreaker@gmail.com>
2022-04-15 20:41:28 -0500

    actualizaciones a react 18, webpack 5, webpack-dev-server 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,121 @@
-const path=require('path');
+const path = require('path');
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+/**@type {import('webpack').Configuration} */
 
+const isProduction = process.env.MODE === 'production';
 
-module.exports ={
-    entry:'./src/index.js',
-    output:{
-        path:path.resolve(__dirname,'build'),
-        filename:'js/[name].[contenthash].js',
+const styleLoader = ["css-loader","sass-loader"];
+styleLoader.unshift(isProduction ? MiniCssExtractPlugin.loader : 'style-loader');
+
+const plugins = [
+    new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html',
+        minify: false,
+        inject: 'body',
+        title: isProduction ? 'Production' : 'Development'
+    }),
+    new webpack.ProvidePlugin({
+        "React": "react"
+    }),
+];
+if (isProduction) {
+    plugins.unshift(
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: 'styles/[name].[contenthash].css' }),
+        new BundleAnalyzerPlugin()
+    );
+} else {
+    plugins.unshift(
+        new webpack.HotModuleReplacementPlugin()
+    );
+}
+
+const optimization = { runtimeChunk: true };
+if (isProduction) {
+    optimization.minimize = true;
+    optimization.splitChunks = {
+        chunks: 'all',
+    },
+        optimization.runtimeChunk = {
+            name: (entrypoint) => `runtimechunk~${entrypoint.name}`
+        };
+}
+
+const devServer = {
+    static: {
+        directory: path.join(__dirname, 'build'),
+        watch: true
+    },
+    compress: true,
+    historyApiFallback: true,
+    port: 1197,
+    hot: true,
+    proxy: {
+        "/api": {
+            target: "http://localhost:80",
+            secure: false,
+            changeOrigin: true
+        }
+    }
+}
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/[name].[contenthash].js',
         clean: true
     },
-    mode:'production',
-    devtool:"source-map",
-    module:{
-        rules:[
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'eval-source-map',
+    module: {
+        rules: [
             {
-                test:/\.(js|jsx)$/,
-                exclude:/node_modules/,
-                use:{
-                    loader:'babel-loader'
+                test: /\.(js(|x))$/,
+                use: {
+                    loader: 'babel-loader'
                 },
-                exclude:[/node_modules/,/dist/,/build/],
+                exclude: [/node_modules/, /dist/, /build/],
 
             },
             {
-                test:/\.html$/,
-                use:'html-loader',
+                test: /\.html$/,
+                use: 'html-loader',
 
             },
             {
-                test:/\.css$/,
-                use:[
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ],
+                test: /\.(s|)css$/,
+                use: styleLoader
 
             },
             {
-                test:/\.(png|jpg|gif|woff|woff2|eot|ttf|svg)(\?[\=\.a-z0-9]+)?$/,
-                use:{
-                    loader: "file-loader",
-                    options:{
-                        name:'[name].[ext]',
-                        outputPath: '/files/',
-                        publicPath:'/application/app/files'
-                    }
+                test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)(\?[\=\.a-z0-9]+)?$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/[name].[ext]'
                 }
             }
         ]
     },
-    plugins:[
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template:"./public/index.html",
-            filename:"./index.html",
-            minify:true,
-            inject:'body'
-        }),
-        new MiniCssExtractPlugin({
-            filename:'styles/[name].[contenthash].css'
-        }),
-        new webpack.ProvidePlugin({
-            "React":"react"
-        }),
-        new BundleAnalyzerPlugin()
-    ],
-    resolve:{
-        extensions:['.js','.jsx'],
-        alias:{
-            "@components":path.resolve(__dirname,'./src/components/'),
-            "@contexts":path.resolve(__dirname,'./src/context/'),
-            "@images":path.resolve(__dirname,'./src/images/'),
-            "@configutarions":path.resolve(__dirname,'./src/config/')
+    plugins: plugins,
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            "@components": path.resolve(__dirname, './src/components/'),
+            "@images": path.resolve(__dirname, './src/images/'),
+            "@configutarions": path.resolve(__dirname, './src/config/'),
+            "@hooks": path.resolve(__dirname, './src/hooks/'),
+            "@contexts": path.resolve(__dirname, './src/contexts/'),
+            "@helpers": path.resolve(__dirname, './src/helpers/'),
+            "@views": path.resolve(__dirname, './src/views/')
         }
     },
-    optimization:{
-        minimize:true,
-        runtimeChunk:{
-            name:(entrypoint)=>`runtimechunk~${entrypoint.name}`
-        },
-        splitChunks:{
-            chunks:'all'
-        }
-    }
+    optimization: optimization,
+    ...(isProduction ? {} : { devServer: devServer })
 };
